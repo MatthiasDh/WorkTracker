96be1fbed0eaeddcc8677b671a9886a7f6b60542
Matthias D'Haeseleer <matthias.dhaeseleer.w0679@student.hogent.be>
2017-09-29 14:48:20 +0200

    Added authentication + registration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,9 @@
 import { Component } from '@angular/core';
 
-// Import the DataService
-import { DataService } from './data.service';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   moduleId: module.id,
+   selector: 'app',
+   templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  
-  // Define a users property to hold our user data
-  users: Array<any>;
 
-  // Create an instance of the DataService through dependency injection
-  constructor(private _dataService: DataService) {
-
-    // Access the Data Service's getUsers() method we defined
-    this._dataService.getUsers()
-        .subscribe(res => this.users = res);
-  }
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+/* import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import { DataService } from './data.service';
 import { LoginComponent } from './login/login.component';
+import { CustomerOverviewComponent } from './customer-overview/customer-overview.component';
+import { CustomersOverviewComponent } from './customers-overview/customers-overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CustomerOverviewComponent,
+    CustomersOverviewComponent
   ],
   imports: [
     BrowserModule,
@@ -19,4 +23,49 @@ import { LoginComponent } from './login/login.component';
   providers: [DataService], // <-Add DataService
   bootstrap: [AppComponent]
 })
+export class AppModule { } */
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+ 
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+ 
+import { customHttpProvider } from './_helpers/custom-http';
+import { AlertComponent } from './_directives/alert.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { AlertService } from './_services/alert.service';
+import { AuthenticationService } from './_services/authentication.service';
+import { UserService } from './_services/user.service';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+ 
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
+    providers: [
+        customHttpProvider,
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        UserService
+    ],
+    bootstrap: [AppComponent]
+})
+ 
 export class AppModule { }
